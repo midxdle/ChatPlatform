88eae17fb6aa192f1172c8132195116cfa7160fa
midxdle <sina.heydarian.sh@gmail.com>
2021-09-06 13:07:42 +0430

    updated

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const server = require('http').createServer(app);
 const io = require("socket.io")(server);
-server.listen(3000);
+require('dotenv').config()
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
 console.log('server is running...');
 var usernames = [];
 
